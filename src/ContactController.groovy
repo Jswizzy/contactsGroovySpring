4c7003c8f439d84ea6ac213b52d6ed85f6612d96
jswizzy <smithjustinn@hotmail.com>
2017-12-31 11:45:53 -0500

    Initial commit

---
+++ src/ContactController.groovy
@@ -0,0 +1,25 @@
+@Grab("thymeleaf-spring4")
+@Grab("spring-boot-starter-actuator")
+
+
+@Controller
+@RequestMapping("/")
+class ContactController {
+
+    @Autowired
+    ContactRepository contactRepo
+
+    @RequestMapping(method=RequestMethod.GET)
+    String home(Map<String,Object> model) {
+        List<Contact> contacts = contactRepo.findAll()
+        model.putAll([contacts: contacts])
+        "home"
+    }
+
+    @RequestMapping(method=RequestMethod.POST)
+    String submit(Contact contact) {
+        contactRepo.save(contact)
+        "redirect:/"
+    }
+}
+ 
